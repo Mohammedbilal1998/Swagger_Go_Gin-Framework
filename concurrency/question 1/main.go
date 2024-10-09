878730475ee3b82e9927ab9e57b831797036826e
Mohammedbilal1998 <35485119+Mohammedbilal1998@users.noreply.github.com>
2024-10-09 08:22:53 +0000

    added one interview question

---
+++ concurrency/question 1/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+func main() {
+	ch := make(chan *int, 4)
+	array := []int{1, 2, 3, 4,5,6,7,8,9,10}
+	wg := sync.WaitGroup{}
+	// Add the number of works equal to the number of array elements.
+	wg.Add(len(array))
+	go func() {
+	  for _, value := range array {
+		ch <- &value
+	  }
+	}()
+	go func() {
+	  for value := range ch {
+		fmt.Println(*value)
+		// Decrement the waitgroup counter with each iteration.
+		wg.Done()
+	  }
+	}()
+  
+	wg.Wait()
+  }
